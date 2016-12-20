e5155d5571e239f357f11c1c4ff3b622ddd94a17
Chris Halpert <chalpert@squarespace.com>
2016-12-19 22:49:28 -0500

    added checkins

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -49,5 +49,10 @@ def analysis_review_avg(business_id):
 def analysis_top_words(business_id):
     return analysis.get_top_words(business_id, n=30)
 
+@app.route('/analysis/checkins/<business_id>')
+def analysis_checkins(business_id):
+    return analysis.get_checkins(business_id)
+
+    
 if __name__ == '__main__':
     app.run()
--- flaskapp/spark_scripts/analysis.py
+++ flaskapp/spark_scripts/analysis.py
@@ -71,16 +71,52 @@ def get_business_info(business_id):
     df = spark.sql(
         """select * from business where business_id = '{business_id}'""".format(business_id=business_id)).toPandas()
 
-    checkin_file = '{}yelp_academic_dataset_checkin.json'.format(YELP_DATA_DIR)
-    checkins = pd.read_json(checkin_file, orient='records', lines=True)
+    checkin_file = '{}total_checkins.csv'.format(YELP_DATA_DIR)
+    checkins = pd.read_csv(checkin_file)
     checkins = checkins[checkins['business_id'] == business_id]
+
     try:
-        checkins['checkins'] = checkins['checkin_info'].apply(lambda x: sum(x.values()))
-        df['checkins'] = checkins['checkins'].values[0]
+        df['checkins'] = checkins['total_checkins'].values[0]
     except:
         df['checkins'] = 0
     
     return df
+
+def get_checkins(business_id):
+    checkin_file = '{}yelp_academic_dataset_checkin.json'.format(YELP_DATA_DIR)
+    checkin_df = pd.read_json(checkin_file, orient='records', lines=True)
+    info = checkin_df.loc[lambda x: x['business_id'] == business_id, 'checkin_info']
+
+    checkins = pd.DataFrame.from_dict(info.values[0], orient='index')
+    checkins = checkins.reset_index()
+    checkins.columns = ['time', 'checkins']
+    checkins['hour'] = checkins['time'].str.split('-').str.get(0)
+    checkins['day'] = checkins['time'].str.split('-').str.get(1)
+
+    checkins_by_day = (checkins
+                       .groupby('day')
+                       .sum()
+                       .reindex(index=['0', '1', '2', '3', '4', '5', '6'])
+                       .fillna(0)
+                       )
+    checkins_by_day.index = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
+    checkins_by_day = checkins_by_day.reset_index()
+    checkins_by_day.columns = ['day', 'checkins']
+
+    checkins_by_hour = (checkins
+                       .groupby('hour')
+                       .sum()
+                       )
+    checkins_by_hour.index = checkins_by_hour.index.astype('int')
+    checkins_by_hour = (checkins_by_hour
+     .reindex(index=range(24))
+     .fillna(0)
+     .reset_index()
+     )
+
+    return {'day': checkins_by_day.to_csv(None, sep='\t', header=True, index=False), 
+            'hour': checkins_by_hour.to_csv(None, sep='\t', header=True, index=False)}
+
 
 def get_top_words(business_id, n):
     word_freq = pd.read_json('{}wordfreq_bybusinessid_bigram.json'.format(YELP_DATA_DIR), orient='records')
