4ec578c35c45e8664084a29aba978c107de62e06
Chris Halpert <chalpert@squarespace.com>
2016-12-21 00:39:12 -0500

    split good and bad word clouds3

--- flaskapp/spark_scripts/analysis.py
+++ flaskapp/spark_scripts/analysis.py
@@ -139,11 +139,12 @@ def get_top_words(business_id, n, kind='all'):
     spark = yelp_lib.spark
     review = yelp_lib.get_parq('review')
     business_df = review.filter(review['business_id'] == business_id)
+    
     if kind == 'good':
         business_df = business_df.filter(business_df['stars'] >= 4)
     elif kind == 'bad':
         business_df = business_df.filter(business_df['stars'] < 3)
-    # tokenizer = Tokenizer(inputCol="text", outputCol="words")
+
     regexTokenizer = RegexTokenizer(inputCol="text", outputCol="words", pattern="\\W")
     wordsDataFrame = regexTokenizer.transform(business_df)
 
