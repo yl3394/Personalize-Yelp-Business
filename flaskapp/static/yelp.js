3ef79f238168171eff14a7137c407d41df5c3a99
Vlad S <vlad.scherbich@gmail.com>
2016-12-22 18:45:18 +0000

    working sort on the table with a few issues

--- flaskapp/static/yelp.js
+++ flaskapp/static/yelp.js
@@ -8,14 +8,21 @@ function getBusinessNames(name) {
 
     theUrl = URL_BASE + 'business/get_name/' + name;
 
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-	    createTable("bizOutput", xmlHttp.responseText);
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            createTable("bizOutput", xmlHttp.responseText);
+
+            // todo:
+            var tbl = $("#bizOutputTable");
+            tbl.tablesorter();
+        }
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
 
-    document.getElementById("favorites").remove();
+    faves = document.getElementById("favorites");
+    if (faves != null)
+        faves.remove();
 }
 
 function createCol(tr, title) {
@@ -30,45 +37,88 @@ function createTable(elemId, response) {
 
     // clear table
     while (elem.firstChild)
-	elem.removeChild(elem.firstChild);
+        elem.removeChild(elem.firstChild);
 
     arr = response.split(BIZ_SEPARATOR);
     if (arr.length == 0 || arr[0].length == 0) {
-	elem.innerHTML = "No results found";
-	return;
+        elem.innerHTML = "No results found";
+        return;
     }
 
     var tbl = document.createElement('table');
     tbl.setAttribute("id", "bizOutputTable");
-    tbl.style.width  = '100px';
+
+    // todo:
+    tbl.setAttribute("class", "tablesorter");
+
+    tbl.style.width = '100px';
     tbl.style.border = '1px solid black';
 
     // create headers
-    var tr = tbl.insertRow();
-    createCol(tr, 'Name');
-    createCol(tr, 'City');
-    createCol(tr, 'State');
-    createCol(tr, 'Avg. Rating');
-    createCol(tr, 'Num. Reviews');
-    createCol(tr, 'Categories');
+    var tblHeader = document.createElement("thead");
+    tbl.appendChild(tblHeader);
 
-    for (var i = 0; i < arr.length; ++i) {
-	tmp = arr[i].split(';');
+    // var tr = tbl.insertRow();
+    var tr = tblHeader.insertRow();
+
+    // createCol(tr, 'Name');
+    // createCol(tr, 'City');
+    // createCol(tr, 'State');
+    // createCol(tr, 'Avg. Rating');
+    // createCol(tr, 'Num. Reviews');
+    // createCol(tr, 'Categories');
+
+
+    // // Name
+    th = document.createElement('th');
+    th.innerHTML = "Name";
+    tr.appendChild(th);
+
+    // City
+    th = document.createElement('th');
+    th.innerHTML = "City";
+    tr.appendChild(th);
+
+    // State
+    th = document.createElement('th');
+    th.innerHTML = "State";
+    tr.appendChild(th);
+
+    // Avg. Rating
+    th = document.createElement('th');
+    th.innerHTML = "Avg. Rating";
+    tr.appendChild(th);
+
+    // Num. Reviews
+    th = document.createElement('th');
+    th.innerHTML = "Num. Reviews";
+    tr.appendChild(th);
+
+    // Categories
+    th = document.createElement('th');
+    th.innerHTML = "Categories";
+    tr.appendChild(th);
+
+    var tblBody = document.createElement("tbody");
+    tbl.appendChild(tblBody);
+
+    for (var i = 1; i < arr.length; ++i) {
+        tmp = arr[i].split(';');
         bizName = tmp[0];
-        bizID =   tmp[1];	
+        bizID = tmp[1];
 
-	var tr = tbl.insertRow();
+        var tr = tblBody.insertRow();
 
-	// create hyperlink with restaurant ID
-	chartURL = URL_BASE + 'business/build_chart/' + bizID;
-	urlHtml = '<a href="' + chartURL + '" target="_blank">' + bizName + '</a>';
-	createCol(tr, urlHtml);
+        // create hyperlink with restaurant ID
+        chartURL = URL_BASE + 'business/build_chart/' + bizID;
+        urlHtml = '<a href="' + chartURL + '" target="_blank">' + bizName + '</a>';
+        createCol(tr, urlHtml);
 
-	createCol(tr, tmp[2]);  // city
-	createCol(tr, tmp[3]);	// state
-	createCol(tr, tmp[4]);  // stars
-	createCol(tr, tmp[5]);  // review count
-	createCol(tr, tmp[6]);  // categories
+        createCol(tr, tmp[2]);  // city
+        createCol(tr, tmp[3]);	// state
+        createCol(tr, tmp[4]);  // stars
+        createCol(tr, tmp[5]);  // review count
+        createCol(tr, tmp[6]);  // categories
     }
 
     elem.appendChild(tbl);
